c27e42c4c1651b44c15867eb09933b0de9cdd569
kolyagae <k.e.l@tut.by>
2024-05-18 12:07:24 +0300

    feat: add mantine theme settings

--- theme.ts
+++ theme.ts
@@ -1,7 +1,162 @@
 "use client";
 
-import { createTheme } from "@mantine/core";
+import {
+  Button,
+  Loader,
+  MultiSelect,
+  NumberInput,
+  Pagination,
+  Paper,
+  Select,
+  createTheme,
+} from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  fontFamily: "Inter, sans-serif",
+  components: {
+    Button: Button.extend({
+      defaultProps: {
+        color: "#9854F6",
+        variant: "filled",
+        radius: "md",
+      },
+      styles: {
+        root: {
+          "--button-hover": "#BD93F7",
+        },
+      },
+    }),
+    Select: Select.extend({
+      defaultProps: {
+        rightSectionWidth: 46,
+        labelProps: { pb: 8, fw: 700 },
+        size: "md",
+        radius: "md",
+        withCheckIcon: false,
+      },
+      styles: {
+        input: {
+          "--input-bd-focus": "#9854F6",
+        },
+        option: {
+          borderRadius: 8,
+          padding: 8,
+        },
+      },
+    }),
+    MultiSelect: MultiSelect.extend({
+      defaultProps: {
+        rightSectionWidth: 46,
+        labelProps: { pb: 8, fw: 700 },
+        size: "md",
+        radius: "md",
+        withCheckIcon: false,
+      },
+      styles: {
+        input: {
+          "--input-bd-focus": "#9854F6",
+        },
+        pill: {
+          backgroundColor: "transparent",
+          padding: 0,
+        },
+        pillsList: {
+          gap: 2,
+          flexWrap: "nowrap",
+          overflow: "hidden",
+        },
+        option: {
+          borderRadius: 8,
+          padding: 8,
+        },
+      },
+    }),
+    NumberInput: NumberInput.extend({
+      defaultProps: {
+        size: "md",
+        radius: "md",
+        labelProps: { pb: 8, fw: 700 },
+        w: 137.8,
+        min: 0,
+        max: 10,
+      },
+      styles: {
+        controls: { width: 30, paddingTop: 8, paddingBottom: 8 },
+        control: {
+          height: 32,
+          border: 0,
+          "--ni-chevron-size": 12,
+          color: "#ACADB9",
+        },
+        input: {
+          "--input-bd-focus": "#9854F6",
+        },
+      },
+    }),
+    Paper: Paper.extend({
+      styles: {
+        root: {
+          "--paper-radius": "12px",
+        },
+      },
+    }),
+    Pagination: Pagination.extend({
+      defaultProps: {
+        radius: "sm",
+        boundaries: -1,
+        siblings: 1,
+      },
+      styles: {
+        root: {
+          "--pagination-active-bg": "#9854F6",
+        },
+        dots: {
+          display: "none",
+        },
+      },
+    }),
+    Loader: Loader.extend({
+      defaultProps: {
+        size: "lg",
+        type: "dots",
+        color: "#9854F6",
+      },
+    }),
+  },
+  breakpoints: {
+    xs: "320px",
+    sm: "600px",
+    md: "900px",
+    lg: "1200px",
+    xl: "1440px",
+  },
+  colors: {
+    purple: [
+      "#F2EBF9",
+      "#E5D5FA",
+      "#D1B4F8",
+      "#BD93F7",
+      "#9854F6",
+      "#541F9D",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
+    grey: [
+      "#FFFFFF",
+      "#F5F5F6",
+      "#EAEBED",
+      "#D5D6DC",
+      "#ACADB9",
+      "#7B7C88",
+      "#000000",
+      "",
+      "",
+      "",
+      "",
+    ],
+    yellow: ["#FAB005", "", "", "", "", "", "", "", "", "", ""],
+  },
 });
